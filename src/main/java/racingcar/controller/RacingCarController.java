4a6944c36628f8bcdc305031ab315adbfcfc87f1
Giyoul <cla12093@gmail.com>
2025-10-25 23:16:16 +0900

    feat(console): get attempt count by user

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -19,5 +19,6 @@ public class RacingCarController {
         inputValidator.checkEmptyInput(carName);
 
         consoleOutput.attemptCountPrompt();
+        String attemptCount = Console.readLine();
     }
 }
