8d974507fb980a9346e404d76690ce7c996cad62
Giyoul <cla12093@gmail.com>
2025-10-25 20:08:12 +0900

    feat(console): get car name input by user

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,16 +1,19 @@
 package racingcar.controller;
 
+import racingcar.view.ConsoleInput;
 import racingcar.view.ConsoleOutput;
 
 public class RacingCarController {
     private final ConsoleOutput consoleOutput;
+    private final ConsoleInput consoleInput;
 
     public RacingCarController() {
         this.consoleOutput = new ConsoleOutput();
+        this.consoleInput = new ConsoleInput();
     }
 
     public void run(){
         consoleOutput.printCarNamePrompt();
-
+        String carNameInput = consoleInput.getCarName();
     }
 }
--- src/main/java/racingcar/view/ConsoleInput.java
+++ src/main/java/racingcar/view/ConsoleInput.java
@@ -1,4 +1,9 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class ConsoleInput {
+    public String getCarName() {
+        return Console.readLine();
+    }
 }
