7efed88aaf7f24593fe77266d684c79b99b3ae83
Giyoul <cla12093@gmail.com>
2025-10-25 17:46:46 +0900

    refactor(test): remove unnecessary import

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
