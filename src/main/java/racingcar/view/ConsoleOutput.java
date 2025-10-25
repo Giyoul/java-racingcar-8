fa0be58a16ca45022e943ca2ebc3b80e63b5ace0
Giyoul <cla12093@gmail.com>
2025-10-26 00:16:30 +0900

    fix(console): fix double newline when printing starting prompt

--- src/main/java/racingcar/view/ConsoleOutput.java
+++ src/main/java/racingcar/view/ConsoleOutput.java
@@ -6,11 +6,11 @@ import racingcar.domain.RacingCar;
 
 public class ConsoleOutput {
     public void carNamePrompt(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void attemptCountPrompt() {
-        System.out.println("시도할 횟수는 몇 회인가요?\n");
+        System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
     public void resultStart(){
