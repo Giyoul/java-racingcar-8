8553d2ebef718284ec993aaa533034b191408ace
Giyoul <cla12093@gmail.com>
2025-10-26 17:45:40 +0900

    refactor: rename exception throw function
    added action "throw" in fucntion name

--- src/main/java/racingcar/helper/RacingCarExceptionHelper.java
+++ src/main/java/racingcar/helper/RacingCarExceptionHelper.java
@@ -8,11 +8,11 @@ public class RacingCarExceptionHelper {
         throw new IllegalArgumentException("이름의 길이는 5를 넘어갈 수 없습니다.");
     }
 
-    public static void emptyInputException(){
+    public static void throwEmptyInputException(){
         throw new IllegalArgumentException("입력은 빈 문자열일 수 없습니다");
     }
 
-    public static void invalidAttemptCountException(){
+    public static void throwInvalidAttemptCountException(){
         throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
     }
 }
--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -5,13 +5,13 @@ import racingcar.helper.RacingCarExceptionHelper;
 public class InputValidator {
     public void checkEmptyInput(String input) {
         if (input.trim().isEmpty()) {
-            RacingCarExceptionHelper.emptyInputException();
+            RacingCarExceptionHelper.throwEmptyInputException();
         }
     }
 
     public void isNumber(String input) {
         if(!input.matches("\\d+")){
-            RacingCarExceptionHelper.invalidAttemptCountException();
+            RacingCarExceptionHelper.throwInvalidAttemptCountException();
         }
     }
 }
