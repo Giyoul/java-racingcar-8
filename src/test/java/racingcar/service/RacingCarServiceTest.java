9d3460870dec885e020d5007168d096f52e24694
Giyoul <cla12093@gmail.com>
2025-10-26 19:22:57 +0900

    test(test): add RacingCarService test
    Add following tests:
    parsing car names
    finding cowinners

---
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -0,0 +1,33 @@
+package racingcar.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RacingCar;
+
+public class RacingCarServiceTest {
+    private RacingCarService racingCarService;
+
+    @BeforeEach
+    void setUp() {
+        racingCarService = new RacingCarService();
+    }
+
+    @Test
+    void 자동차_이름_파싱_테스트() {
+        List<RacingCar> cars = racingCarService.parseCarNames("pobi,woni");
+        assertThat(cars).hasSize(2);
+    }
+
+    @Test
+    void 공동_우승자_찾기_테스트() {
+        List<RacingCar> cars = List.of(
+                new RacingCar("pobi"),
+                new RacingCar("woni")
+        );
+        List<RacingCar> winners = racingCarService.findWinners(cars);
+        assertThat(winners).hasSize(2);
+    }
+}
