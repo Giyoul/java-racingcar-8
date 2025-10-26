99748f62887f29a131f6b698fced03ffd7750266
Giyoul <cla12093@gmail.com>
2025-10-26 17:01:36 +0900

    refactor(validation): move exceptions to exception helper
    empty input exception in input validator
    invalid attempt count input exception in input validator

--- src/main/java/racingcar/helper/RacingCarExceptionHelper.java
+++ src/main/java/racingcar/helper/RacingCarExceptionHelper.java
@@ -4,7 +4,15 @@ public class RacingCarExceptionHelper {
     private RacingCarExceptionHelper() {
     }
 
-    public static IllegalArgumentException racingCarNameLengthException() {
+    public static void racingCarNameLengthException() {
         throw new IllegalArgumentException("이름의 길이는 5를 넘어갈 수 없습니다.");
+    }
+
+    public static void emptyInputException(){
+        throw new IllegalArgumentException("입력은 빈 문자열일 수 없습니다");
+    }
+
+    public static void invalidAttemptCountException(){
+        throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
     }
 }
--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,15 +1,17 @@
 package racingcar.validator;
 
+import racingcar.helper.RacingCarExceptionHelper;
+
 public class InputValidator {
     public void checkEmptyInput(String input) {
         if (input.trim().isEmpty()) {
-            throw new IllegalArgumentException("입력은 빈 문자열일 수 없습니다.");
+            RacingCarExceptionHelper.emptyInputException();
         }
     }
 
     public void isNumber(String input) {
         if(!input.matches("\\d+")){
-            throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
+            RacingCarExceptionHelper.invalidAttemptCountException();
         }
     }
 }
