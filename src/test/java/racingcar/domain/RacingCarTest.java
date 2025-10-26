6b4dd783175fc4922e62589073a6a012438626a6
Giyoul <cla12093@gmail.com>
2025-10-26 19:10:58 +0900

    test(test): add RacingCar object creation and move function test

---
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+    @Test
+    void 자동차_객체_생성_및_움직임_테스트() {
+        RacingCar car = new RacingCar("pobi");
+        car.move();
+        assertThat(car.getPosition()).isEqualTo(1);
+        assertThat(car.getName()).isEqualTo("pobi");
+    }
+}
